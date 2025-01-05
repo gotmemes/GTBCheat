a6bff0ffa39194477f4e8cdb24a3cb2fa4c6594c
gotmemes <46362694+gotmemes@users.noreply.github.com>
2025-01-05 15:32:05 -0500

    multi line hud

--- src/main/java/org/polyfrost/example/hud/TestHud.java
+++ src/main/java/org/polyfrost/example/hud/TestHud.java
@@ -1,20 +1,23 @@
 package org.polyfrost.example.hud;
 
-import cc.polyfrost.oneconfig.hud.SingleTextHud;
+import cc.polyfrost.oneconfig.hud.TextHud;
 import org.polyfrost.example.config.TestConfig;
+import java.util.List;
 
 /**
  * An example OneConfig HUD that is started in the config and displays text.
  *
  * @see TestConfig#hud
  */
-public class TestHud extends SingleTextHud {
+public class TestHud extends TextHud {
     public TestHud() {
-        super("Test", true);
+        super(true, 0, 0);
     }
 
     @Override
-    public String getText(boolean example) {
-        return "I'm an example HUD";
+    protected void getLines(List<String> lines, boolean example) {
+        lines.add("I'm line 1 of the example HUD");
+        lines.add("I'm line 2 of the example HUD");
+        lines.add("I'm line 3 of the example HUD");
     }
 }
